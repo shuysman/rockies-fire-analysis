7d7b10773df4e92e34e9e1dccb06c898c1a38488
Stephen Huysman <shuysman@gmail.com>
2023-05-11 15:26:58 -0600

    fix scripts

--- gridmet-historical.R
+++ gridmet-historical.R
@@ -1,1 +1,37 @@
-/media/steve/storage/waterbalance/fire/gridmet-historical.R
+## Extract historical daily tp  data from gridMET for fire ignition sites
+library(tidyverse)
+library(sf)
+library(ncdf4)
+library(raster)
+library(cmsafops)
+
+
+## Gridmet variable names used by urls
+gridmet_vars = c("pr",            ## Precipitation (mm)
+                 "tmmx",          ## Tmax (K)
+                 "tmmn",          ## Tmin (K)
+                 "rmax",          ## RH Max(%)
+                 "rmin",          ## RH Min (%)
+                 "vpd")           ## VPD kPa
+
+
+## Gridmet variable labels used internally on ncdf4 files
+gridmet_labels <- c("precipitation_amount",
+                    "daily_maximum_temperature",
+                    "daily_minimum_temperature",
+                    "daily_maximum_relative_humidity",
+                    "daily_minimum_relative_humidity",
+                    "daily_mean_vapor_pressure_deficit")
+
+
+
+sites <- read.csv('./Southern_rockies_fire_points.csv')
+
+##for (i in 1:length(gridmet_vars)) {
+for (i in 6) {
+    url <- paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_met_", gridmet_vars[i], "_1979_CurrentYear_CONUS.nc#fillmismatch")
+    ds <- nc_open(url)
+    selpoint.multi(gridmet_labels[i], nc=ds, outpath = paste0("./gridmet-",gridmet_vars[i]), lon1 = sites$Lon, lat1 = sites$Lat, station_names = sites$Event_ID, format = "csv", verbose = TRUE)
+}
+
+
--- merge-csv.R
+++ merge-csv.R
@@ -1,1 +1,66 @@
-/media/steve/storage/waterbalance/fire/merge-csv.R
+## Merge together csvs for fire ignition analysis
+## fire-tp and fire-wb data dirs from Mike Tercek
+## gridmet-vpd data pulled from gridmet thredds server, using maca-vpd.R script
+
+library(tidyverse)
+library(parallel)
+
+sites <- read_csv('./rockies_fire_points.csv')
+out_dir <- "./out/"
+t_base <- 5.5 ## tbase in degrees C for GDD calculations
+skip <- sites$Event_ID %in% c("WY4352911071419870423") ## file missing for this site, need to get Mike to rerun, or pull myself,
+##  but this site is not a wildfire site so probably not relevant to analysis
+
+f_to_c <- function(t_f) {
+    return((t_f - 32) * 5/9)
+}
+
+generate_wb <- function(site) {
+    tp <- read_csv(paste('./fire_tp/', site, '_historical.csv', sep=''), show_col_types = FALSE)
+    wb <- read_csv(paste('./fire_wb/', site, '_water_balance_historical.csv', sep=''), show_col_types = FALSE)
+    vpd <- read_delim(delim=";", paste('./gridmet-vpd/', site, '.csv', sep=''), show_col_types = FALSE) %>% filter(!row_number() >= 15707) ## cut off extra rows, Mike's data only goes to 2021-12-31, but vpd data we pulled is more recent so row # mismatched
+
+    out_df <- bind_cols(tp, wb, vpd) %>%
+        select(Date = "Date...1",
+               P = "Precip (in)",
+               TmaxF = "Tmax (F)",
+               TminF = "Tmin (F)",
+               RHmax = "RHmax (%)",
+               RHmin = "RHmin(%)",
+               TavgF = "Tavg (F)",
+               D = "Deficit in",
+               AET = "AET in",
+               SOIL = "soil_water in",
+               DRO = "runoff in",
+               RAIN = "rain in",
+               PACK = "accumswe in",
+               PET = "PET in",
+               VPD = "result") %>% ## vpd in kPa
+        mutate(P = P * 25.4, # in to mm
+               D = D * 25.4,
+               AET = AET * 25.4,
+               SOIL = SOIL * 25.4,
+               DRO = DRO * 25.4,
+               RAIN = RAIN * 25.4,
+               PACK = PACK * 25.4,
+               PET = PET * 25.4,
+               RH = (RHmax + RHmin) /2,
+               Tmax = f_to_c(TmaxF),
+               Tmin = f_to_c(TminF),
+               T = f_to_c(TavgF),
+               "/\\SOIL" = SOIL - lag(SOIL, default = 0),
+               GDD1 = (f_to_c(TmaxF) + f_to_c(TminF)) / 2 - t_base,
+               GDD = ifelse(GDD1 >= 0, GDD1, 0)) %>% ## GDD cannot be negative, so minimum is 0
+        select(-GDD1, -TmaxF, -TminF, -TavgF, -RHmax, -RHmin) 
+               
+
+    write.csv(out_df, paste(out_dir, site, ".csv", sep=""))
+}
+
+# Remove !skip after getting missing site from Mike
+for (site in sites$Event_ID[!skip]) {
+    generate_wb(site)
+}
+
+
+system.time(save1 <- mclapply(sites$Event_ID[!skip], generate_wb, mc.cores = 8))
