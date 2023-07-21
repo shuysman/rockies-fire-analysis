b763c61ed504fefc3185b1837d6fa7284f52a3e6
Stephen Huysman <shuysman@gmail.com>
2023-07-21 17:42:12 -0600

    version to run on cluster

--- projections-terra.R
+++ projections-terra.R
@@ -1,30 +1,39 @@
 library(dplyr)
+##library(purrr)
 library(ggplot2)
 library(parallel)
+library(iterators)
+##library(foreach)
 library(lubridate)
 library(terra)
+library(zoo)
 
-terraOptions(verbose = TRUE)
+terraOptions(verbose = TRUE,
+             memfrac = 0.7)
 
 rolling_window <- 7
 threshold <- 0.1
 fire_season_start <- 93
 fire_season_end <- 289
 ## ncpath <- "/home/steve/Downloads/thredds/Deficit_NorESM1-M_rcp45_2024subset.nc"
-in_path <- "/home/steve/Downloads/thredds/"
-hist_path <- "/home/steve/Downloads/thredds/historical/"
-out_path <- "/home/steve/Downloads/thredds/out/"
+##in_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/"
+home_path <- Sys.getenv("HOME")
+in_path <- paste0(home_path, "/data/gye/forecasts")
+##hist_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/historical/"
+hist_path <- paste0(home_path, "/data/gye/historical/")
+##out_path <- "/media/smithers/shuysman/data/out/fire/"
+out_path <- "./out/"
 ## nc_data_path <- "/home/steve/Downloads/thredds/"
 
 ## models <- c('NorESM1-M', 'MRI-CGCM3','MIROC-ESM-CHEM', 'MIROC5','IPSL-CM5A-LR','inmcm4','HadGEM2-CC365','CSIRO-Mk3-6-0','CNRM-CM5','CanESM2', 'BNU-ESM','CCSM4', 'GFDL-ESM2G')
 models <- c("NorESM1-M", "MRI-CGCM3", "MIROC5", "IPSL-CM5A-LR", "inmcm4", "HadGEM2-CC365", "CSIRO-Mk3-6-0", "CNRM-CM5", "CanESM2", "BNU-ESM", "CCSM4", "GFDL-ESM2G") ## MIROC-ESM-CHEM >2070 rcp45 futures are missing
-model <- c("NorESM1-M")
+##model <- c("NorESM1-M")
 
 scenarios <- c("rcp85", "rcp45")
-scenario <- c("rcp45")
+##scenario <- c("rcp45")
 
 ## years <- seq(2024, 2050)
-years <- seq(2051, 2099)
+years <- seq(2050, 2075)
 
 ecdf_regression <- function(x) {
     ## ECDF function generated from dryness_script.Rmd.  This function
@@ -36,75 +45,126 @@ ecdf_regression <- function(x) {
     return(e^(-10.38 + 29.98 * x + -45.25 * x^2 + 25.65 * x^3))
 }
 
-## cl <- makeCluster(12)
-## clusterExport(cl, "ecdf_regression")
-## clusterExport(cl, "percent_rank")
-## clusterExport(cl, "if_else")
-## clusterExport(cl, "fire_season_start")
-## clusterExport(cl, "fire_season_end")
-## clusterExport(cl, "threshold")
+## rank_day_historical <- function(wb_day) {
+##     ## Add layer for single day to historical data set, calculate percent_rank and return percent rankile for input day
+##     ## We want to rank each day individually against the history, to see how dry each day in projection is relative
+##     ## to the history
+##     t <- time(wb_day)
+##     r <- c(wb_day, wbdata_historical)
+##     r <- terra::app(r, fun = function(x) percent_rank(x), cores = cl)
+##     r_day <- subset(r, 1)
+##     time(r_day) <- t
+##     return(r_day)
+## }
 
-## start.time <- Sys.time()
+## rolling_sum_ncpaths <- function(ncpaths) {
+##     ## r <- foreach(i = 1:length(ncpaths), .combine = c) %dopar% {
+##     ##     terra::roll(terra::rast(ncpaths[i]), n = rolling_window, fun = sum, type = "to", circular = FALSE)
+##     ## }
+##     ## return(r)
+##     r <- rast()
+##     for (ncpath in ncpaths) {
+##         r2 <- terra::rast(ncpath)
+##         r2_smooth <- terra::roll(r2, n = rolling_window, fun = sum, type = "to", circular = FALSE)
+##         r <- c(r, r2_smooth)
+##     }
+##     return(r)
+## }
+
+cl <- makeCluster(24)
+doParallel::registerDoParallel(cl)
+clusterExport(cl, "ecdf_regression")
+clusterExport(cl, "percent_rank")
+clusterExport(cl, "if_else")
+clusterExport(cl, "fire_season_start")
+clusterExport(cl, "fire_season_end")
+clusterExport(cl, "threshold")
+clusterExport(cl, "rolling_window")
+clusterExport(cl, "rollapply")
+
+start.time <- Sys.time()
+
+ncpaths_historical <- list.files(path = hist_path, pattern = "Deficit.*.nc", full.names = TRUE)
+wbdata_historical <- rast(ncpaths_historical) %>% subset(year(time(.)) <= 2021)
+##wbdata_historical_smoothed <-  terra::roll(wbdata_historical, n = rolling_window, fun = sum, type = "to", circular = FALSE)
+
+wbdata_historical_smoothed <- terra::app(wbdata_historical, fun = function (x) rollapply(x, rolling_window, sum, by = 1, partial = FALSE, fill = NA, align = "right"), cores = cl)
+
+terra::time(wbdata_historical_smoothed) <- terra::time(wbdata_historical)
+
+wbdata_historical_smoothed <- wbdata_historical_smoothed %>%
+    subset(yday(time(.)) >= fire_season_start) %>%
+    subset(yday(time(.)) <= fire_season_end)
 
 for (model in models) {
     print(model)
     for (scenario in scenarios) {
         print(scenario)
 
-        tmpdir <- tempdir(check = TRUE)
-
-        ncpaths_historical <- list.files(path = hist_path, pattern = "Deficit.*.nc", full.names = TRUE)
         ncpaths_future <- list.files(path = in_path, pattern = paste0("Deficit_", model, "_", scenario, ".*.nc"), full.names = TRUE)
+        wbdata_future <- terra::rast(ncpaths_future) %>% subset(year(time(.)) >= 2022)
+        
+        wbdata_percentiles <- rast()
+        for (year in years) {
 
-        wbdata_historical <- terra::rast(ncpaths_historical) %>% subset(year(time(.)) <= 2022)
-        wbdata_future <- terra::rast(ncpaths_future) %>% subset(year(time(.)) >= 2023)
-        wbdata <- c(wbdata_historical, wbdata_future)
+            ##year <- 2025
+            
+            r <- subset(wbdata_future, year(time(wbdata_future)) == year)
 
-        t <- rast(nrows = 4, ncols = 4, extent = ext(wbdata), crs = crs(wbdata))
-        wbdata_tiles <- terra::makeTiles(wbdata, y = t, filename = paste0(tmpdir, "/tile_.tif"), overwrite = TRUE)
+            ##wbdata_future_smoothed <- terra::roll(wbdata_future, n = rolling_window, fun = sum, type = "to", circular = FALSE) %>% subset(any(day(time(.)) >= fire_season_start, day(time(.)) <= fire_season_end))
 
-        ## R's GC sucks
-        rm(list(wbdata_historical, wbdata_future, wbdata))
+            wbdata_future_smoothed <- terra::app(r, fun = function (x) rollapply(x, rolling_window, sum, by = 1, partial = FALSE, fill = NA, align = "right"), cores = cl)
+            
+            terra::time(wbdata_future_smoothed) <- terra::time(r)
+            
+            wbdata_future_smoothed <- wbdata_future_smoothed %>%
+                subset(yday(time(.)) >= fire_season_start) %>%
+                subset(yday(time(.)) <= fire_season_end)
 
-        rlist <- list()
-        for (tile in wbdata_tiles) {
-            r <- terra::rast(tile)
+            r_smoothed_plus_historical <- c(wbdata_future_smoothed, wbdata_historical_smoothed)
+            r_percentiles <- terra::app(r_smoothed_plus_historical,
+                                        fun = function(x) percent_rank(x), cores = cl
+                                        )
 
-            wbdata_smoothed <- terra::roll(r, n = rolling_window, fun = sum, type = "to", circular = FALSE) %>%
-                subset(any(day(time(wbdata_smoothed)) >= fire_season_start, day(time(wbdata_smoothed)) <= fire_season_end))
-
-            wbdata_percentiles <- terra::app(wbdata_smoothed,
-                fun = function(x) percent_rank(x), cores = 1
-            )
-
-            above_fire_risk <- terra::app(wbdata_percentiles, fun = function(x) if_else(ecdf_regression(x) >= threshold, 1, 0), cores = 1)
-            time(above_fire_risk) <- time(wbdata_smoothed)
-
-            days_above_fire_risk <- terra::tapp(above_fire_risk, index = "years", fun = "sum", na.rm = TRUE)
-
-            rlist <- append(rlist, days_above_fire_risk)
+            terra::time(r_percentiles) <- terra::time(r_smoothed_plus_historical)
+            ranked_year <- subset(r_percentiles, year(time(r_percentiles)) == year)
+            wbdata_percentiles <- c(wbdata_percentiles, ranked_year)
+            
         }
-        rsrc <- sprc(rlist)
-        m <- mosaic(rsrc)
+        
+        ##wbdata_future_smoothed <- terra::roll(wbdata_future, n = rolling_window, fun = sum, type = "to", circular = FALSE)
+        ##wbdata_future_smoothed <- rolling_sum_ncpaths(ncpaths_future)
+        
+        ##wbdata_future_smoothed <- wbdata_future_smoothed %>%
+        ##    subset(any(day(time(.)) >= fire_season_start, day(time(.)) <= fire_season_end))
+        
+        ## wbdata_percentiles <- terra::app(wbdata_smoothed,
+        ##     fun = function(x) percent_rank(x), cores = cl
+        ## )
+        
+        above_fire_risk <- terra::app(wbdata_percentiles, fun = function(x) if_else(ecdf_regression(x) >= threshold, 1, 0), cores = cl)
+        terra::time(above_fire_risk) <- terra::time(wbdata_percentiles)
 
+        days_above_fire_risk <- terra::tapp(above_fire_risk, index = "years", fun = "sum", na.rm = TRUE)
 
-        days_above_fire_risk_all_years <- mean(all_years, na.rm = TRUE)
+        days_above_fire_risk_all_years <- mean(days_above_fire_risk, na.rm = TRUE)
 
-        writeCDF(all_years, filename = paste0(out_path, "Days_Above_Fire_Risk_Deficit_", model, "_", scenario, "_", years[1], "-", tail(years, 1), ".nc"), overwrite = TRUE)
+        writeCDF(days_above_fire_risk, filename = paste0(out_path, "Days_Above_Fire_Risk_Deficit_", model, "_", scenario, "_", years[1], "-", tail(years, 1), ".nc"), overwrite = TRUE)
         writeCDF(days_above_fire_risk_all_years, filename = paste0(out_path, "Mean_Days_Above_Fire_Risk_Deficit_", model, "_", scenario, "_", years[1], "-", tail(years, 1), ".nc"), overwrite = TRUE)
 
-        file.remove(tmpdir)
     }
 }
-## stopCluster(cl)
-## end.time <- Sys.time()
-## time.taken <- round(end.time - start.time,2)
-## time.taken
+
+stopCluster(cl)
+
+end.time <- Sys.time()
+time.taken <- round(end.time - start.time,2)
+time.taken
 
 
-ggplot(as.data.frame(days_above_fire_risk_all_years, xy = TRUE)) +
-    geom_raster(mapping = aes(x = x, y = y, fill = mean)) +
-    ##   geom_sf(data = gye_boundary_lcc, alpha = 0.15, fill = NA, lwd = 2) +
-    theme_bw() +
-    scico::scale_fill_scico(palette = "bilbao") +
-    ggtitle("Days above fire danger threshold")
+## ggplot(as.data.frame(days_above_fire_risk_all_years, xy = TRUE)) +
+##     geom_raster(mapping = aes(x = x, y = y, fill = mean)) +
+##     ##   geom_sf(data = gye_boundary_lcc, alpha = 0.15, fill = NA, lwd = 2) +
+##     theme_bw() +
+##     scico::scale_fill_scico(palette = "bilbao") +
+##     ggtitle("Days above fire danger threshold")
