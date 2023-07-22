46de384d4ebf949fffdc978a031518b9ae32d123
Stephen Huysman <shuysman@gmail.com>
2023-07-22 13:49:45 -0600

    fix dirname

--- batch-fire.sh
+++ batch-fire.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FUTURE_FILE_DIR=/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/
+FUTURE_FILE_DIR=${HOME}/data/gye/forecasts/rolling_sum/
 FUTURE_FILES=(${FUTURE_FILE_DIR}/*)
 N_FILES=${#FUTURE_FILES[@]}
 
