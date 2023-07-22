8278190ccdf8d3799137918500f4c24e0dcc668c
Stephen Huysman <shuysman@gmail.com>
2023-07-22 14:14:12 -0600

    fix typo

--- projections-terra.R
+++ projections-terra.R
@@ -81,7 +81,7 @@ for (day in fire_season_start:fire_season_end) {
                               fun = function(x) percent_rank(x), cores = cores
                               )
     terra::time(percentiles) <- terra::time(future_day_plus_historical)
-    ranked_day <- subset(percentiles, yday(time(percentiles)) == yday)
+    ranked_day <- subset(percentiles, yday(time(percentiles)) == day)
     ranked_year <- c(ranked_year, ranked_day)
 }
 
