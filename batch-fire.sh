a06930d380149a4e1d197e75a989b0c5ac6c48b8
Stephen Huysman <shuysman@gmail.com>
2023-07-22 13:58:51 -0600

    more ram per run, less runs

--- batch-fire.sh
+++ batch-fire.sh
@@ -4,5 +4,5 @@ FUTURE_FILE_DIR=${HOME}/data/gye/forecasts/rolling_sum/
 FUTURE_FILES=(${FUTURE_FILE_DIR}/*)
 N_FILES=${#FUTURE_FILES[@]}
 
-sbatch --array=0-$(($N_FILES-1))%20 fire.sbatch
+sbatch --array=0-$(($N_FILES-1))%10 fire.sbatch
 
