d5a3c33ad598f47c2b891f8789147eafd484d781
Stephen Huysman <shuysman@gmail.com>
2023-07-22 13:16:35 -0600

    fix bash arrays

--- batch-fire.sh
+++ batch-fire.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FUTURE_FILE_DIR=/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/
-FUTURE_FILES=${FUTURE_FILE_DIR}/*
-N_FILES=$(echo $FUTURE_FILES | wc -w)
+FUTURE_FILES=(${FUTURE_FILE_DIR}/*)
+N_FILES=${#FUTURE_FILES[@]}
 
 sbatch --array=1-${N_FILES} fire.sbatch
