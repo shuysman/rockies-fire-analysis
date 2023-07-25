634ffd654d471773573d5e77e7513a962eb4a30a
Stephen Huysman <shuysman@gmail.com>
2023-07-25 09:23:32 -0600

    change array task logic

--- batch-fire.sh
+++ batch-fire.sh
@@ -3,5 +3,5 @@
 MODELS=("NorESM1-M" "MRI-CGCM3" "MIROC5" "IPSL-CM5A-LR" "inmcm4" "HadGEM2-CC365" "CSIRO-Mk3-6-0" "CNRM-CM5" "CanESM2" "BNU-ESM" "CCSM4" "GFDL-ESM2G")
 N=${#MODELS[@]}
 
-sbatch --array=0-$((($N*2)-1))%2 fire.sbatch
+sbatch --array=0-$(($N-1))%2 fire.sbatch
 
