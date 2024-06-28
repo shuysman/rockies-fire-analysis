f139e5ee6bcc4485025b6ef9b0b2f27477347c17
Stephen Huysman <shuysman@gmail.com>
2024-06-28 13:02:59 -0600

    set up for cluster

--- projections-terra.R
+++ projections-terra.R
@@ -20,9 +20,9 @@ scenario <- args[2]
 
 ##future_file <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/Deficit_rolling_sum_7_BNU-ESM_rcp45_2033.nc"
 
-model <- "BNU-ESM"
-scenario <- "rcp45"
-year <- 2033
+## model <- "BNU-ESM"
+## scenario <- "rcp45"
+## year <- 2033
 
 print(model)
 print(scenario)
@@ -36,15 +36,15 @@ fire_season_len <- fire_season_end - fire_season_start
 ## ncpath <- "/home/steve/Downloads/thredds/Deficit_NorESM1-M_rcp45_2024subset.nc"
 ##in_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/"
 home_path <- Sys.getenv("HOME")
-##future_path <- paste0(home_path, "/data/gye/forecasts/rolling_sum/")
-future_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/"
-hist_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/historical/rolling_sum/"
+future_path <- paste0(home_path, "/data/nps_gridded_wb/gye/forecasts/rolling_sum/")
+##future_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/"
+hist_path <- paste0(home_path, "/data/nps_gridded_wb/gye/historical/rolling_sum/")
+##hist_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/historical/rolling_sum/"
 ##quants_path <- "/home/steve/OneDrive/nothern-rockies-dryness/out/Deficit_historical_quants_0.79_0.845_0.875_.nc"
-##quants_path <- paste0(home_path, "/data/gye/historical/rolling_sum/Deficit_historical_quants_0.79_0.845_0.875_.nc")
-quants_path <- "/home/steve/OneDrive/northern-rockies-dryness/out/Deficit_historical_quants_0.35_0.425_0.485_.nc"
-##hist_path <- paste0(home_path, "/data/gye/historical/rolling_sum/")
-##out_path <- "/media/smithers/shuysman/data/out/fire/"
-out_path <- "./out/"
+quants_path <- paste0(home_path, "/data/nps_gridded_wb/gye/historical/rolling_sum/Deficit_historical_quants_0.35_0.425_0.485_.nc")
+## quants_path <- "/home/steve/OneDrive/northern-rockies-dryness/out/Deficit_historical_quants_0.35_0.425_0.485_.nc"
+## out_path <- "/media/smithers/shuysman/data/out/fire/"
+out_path <- paste0(home_path, "/out/fire")
 ## nc_data_path <- "/home/steve/Downloads/thredds/"
 
 ## models <- c('NorESM1-M', 'MRI-CGCM3','MIROC-ESM-CHEM', 'MIROC5','IPSL-CM5A-LR','inmcm4','HadGEM2-CC365','CSIRO-Mk3-6-0','CNRM-CM5','CanESM2', 'BNU-ESM','CCSM4', 'GFDL-ESM2G')
@@ -78,13 +78,13 @@ clean_timeseries <- function(x) {
 
 start.time <- Sys.time()
 
-ncpaths_historical_smoothed <- list.files(path = hist_path, pattern = "Deficit.*.nc", full.names = TRUE)
+## ncpaths_historical_smoothed <- list.files(path = hist_path, pattern = "Deficit.*.nc", full.names = TRUE)
 
-wbdata_historical_smoothed <- rast(ncpaths_historical_smoothed) %>%
-    subset(year(time(.)) <= 2021) %>%
-    subset(yday(time(.)) >= fire_season_start) %>%
-    subset(yday(time(.)) <= fire_season_end) %>%
-    terra::app(fun = clean_timeseries, cores = 8)
+## wbdata_historical_smoothed <- rast(ncpaths_historical_smoothed) %>%
+##     subset(year(time(.)) <= 2021) %>%
+##     subset(yday(time(.)) >= fire_season_start) %>%
+##     subset(yday(time(.)) <= fire_season_end) %>%
+##     terra::app(fun = clean_timeseries, cores = 8)
 
 future_files <- Sys.glob(paste0(future_path, "Deficit_rolling_sum_", rolling_window, "_", model, "_", scenario, "_*.nc"))
 
@@ -94,6 +94,7 @@ wbdata_future_smoothed <- terra::rast(future_files) %>%
 
 ## quants <- c(0.79, 0.845, 0.875)
 quants <- c(0.35, 0.425, 0.485)
+
 ############
 #### Generate Quants
 #### Takes a long time to run, so run once and cache the quants file
@@ -105,14 +106,15 @@ quants <- c(0.35, 0.425, 0.485)
 ### ecdf_regression(0.89) = 0.1058749
 ### ecdf_regression(0.915) = 0.1503866
 ### ecdf_regression(0.933) = 0.2036546
-quants_string <- paste0(quants, "_", collapse = "")
-wbdata_historical_quants_file <-  paste0(out_path, "Deficit_historical_quants_", quants_string, ".nc")
+### 
+## quants_string <- paste0(quants, "_", collapse = "")
+## wbdata_historical_quants_file <-  paste0(out_path, "Deficit_historical_quants_", quants_string, ".nc")
 
-start.time <- Sys.time()
-wbdata_historical_quants <- terra::quantile(wbdata_historical_smoothed, probs = quants, na.rm = TRUE, filename = wbdata_historical_quants_file, overwrite = TRUE)
-end.time <- Sys.time()
-time.taken <- round(end.time - start.time,2)
-time.taken
+## start.time <- Sys.time()
+## wbdata_historical_quants <- terra::quantile(wbdata_historical_smoothed, probs = quants, na.rm = TRUE, filename = wbdata_historical_quants_file, overwrite = TRUE)
+## end.time <- Sys.time()
+## time.taken <- round(end.time - start.time,2)
+## time.taken
 #############
 
 wbdata_historical_quants <- terra::rast(quants_path)
@@ -131,38 +133,44 @@ end.time <- Sys.time()
 time.taken <- round(end.time - start.time,2)
 time.taken
 
-ggplot() +
-  geom_spatraster(data = mean(s_0.1))
+## ggplot() +
+##   geom_spatraster(data = mean(s_0.1))
 
-plot(mean(s_0.1))
+## plot(mean(s_0.1))
 
-ggplot() +
-  geom_spatraster(data = s_0.1$y_2050)
-
-
-test_thresh <- wbdata_future_smoothed %>%
-    subset(year(time(.)) == 2033) %>%
-    terra::compare(subset(wbdata_historical_quants, 1), ">")
-
-s_test_thresh <- sum(test_thresh)
-
-plot(s_test_thresh)
-
-plot(max(wbdata_future_smoothed %>%
-    subset(year(time(.)) == 2033)))
+## ggplot() +
+##   geom_spatraster(data = s_0.1$y_2050)
 
 
-test0.1 <- rast("./out/Annual_sum_days_above_fire_risk_threshold_0.1_BNU-ESM_rcp45.nc")
-test0.2 <- rast("./out/Annual_sum_days_above_fire_risk_threshold_0.2_BNU-ESM_rcp45.nc")
+## test_thresh <- wbdata_future_smoothed %>%
+##     subset(year(time(.)) == 2033) %>%
+##     terra::compare(subset(wbdata_historical_quants, 1), ">")
+
+## s_test_thresh <- sum(test_thresh)
+
+## plot(s_test_thresh)
+
+## plot(max(wbdata_future_smoothed %>%
+##     subset(year(time(.)) == 2033)))
 
 
-ggplot() +
-    geom_spatraster(data = mean(test0.1)) +
-    scale_fill_hypso_c(palette = "colombia") +
-    ggtitle("BNU-ESM rcp4.5 Mean annual days above threshold 2023-2099 (10% of historical fires), new %ile algorithm")
+
+## ggplot() +
+##     geom_spatraster(data = wbdata_historical_quants["0.485"]) +
+##     scale_fill_hypso_c(palette = "colombia") 
 
 
-ggplot() +
-    geom_spatraster(data = mean(test0.2)) +
-    scale_fill_hypso_c(palette = "colombia") +
-    ggtitle("BNU-ESM rcp4.5 Mean annual days above threshold 2023-2099 (20% of historical fires), new %ile algorithm")
+## test0.1 <- rast("./out/Annual_sum_days_above_fire_risk_threshold_0.1_BNU-ESM_rcp45.nc")
+## test0.2 <- rast("./out/Annual_sum_days_above_fire_risk_threshold_0.2_BNU-ESM_rcp45.nc")
+
+
+## ggplot() +
+##     geom_spatraster(data = mean(test0.1)) +
+##     scale_fill_hypso_c(palette = "colombia") +
+##     ggtitle("BNU-ESM rcp4.5 Mean annual days above threshold 2023-2099 (10% of historical fires), new %ile algorithm")
+
+
+## ggplot() +
+##     geom_spatraster(data = mean(test0.2)) +
+##     scale_fill_hypso_c(palette = "colombia") +
+##     ggtitle("BNU-ESM rcp4.5 Mean annual days above threshold 2023-2099 (20% of historical fires), new %ile algorithm")
