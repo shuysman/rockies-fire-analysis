5c2450c6c7295d8a16879249993ce9cbcdc07a13
Stephen Huysman <shuysman@gmail.com>
2024-11-10 17:38:52 -0700

    new algo updates

--- src/projections-terra.R
+++ src/projections-terra.R
@@ -28,9 +28,9 @@ print(scenario)
 ##cores <- 12
 rolling_window <- 7
 ##threshold <- 0.1
-fire_season_start <- 93
-fire_season_end <- 289
-fire_season_len <- fire_season_end - fire_season_start
+## fire_season_start <- 93
+## fire_season_end <- 289
+## fire_season_len <- fire_season_end - fire_season_start
 ## ncpath <- "/home/steve/Downloads/thredds/Deficit_NorESM1-M_rcp45_2024subset.nc"
 ##in_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/"
 home_path <- Sys.getenv("HOME")
@@ -38,7 +38,7 @@ future_path <- paste0(home_path, "/data/gye/forecasts/rolling_sum/")
 ##future_path <- paste0("/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/")
 ##hist_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/historical/rolling_sum/"
 ##quants_path <- "/home/steve/OneDrive/nothern-rockies-dryness/out/Deficit_historical_quants_0.79_0.845_0.875_.nc"
-quants_path <- paste0(home_path, "/data/gye/historical/rolling_sum/Deficit_historical_quants_0.79_0.845_0.875_.nc")
+##quants_path <- paste0(home_path, "/data/gye/historical/rolling_sum/Deficit_historical_quants_0.35_0.5_0.7_.nc")
 hist_path <- paste0(home_path, "/data/gye/historical/rolling_sum/")
 ##out_path <- "/media/smithers/shuysman/data/out/fire/"
 out_path <- "./out/"
@@ -50,14 +50,14 @@ out_path <- "./out/"
 ##scenarios <- c("rcp85", "rcp45")
 
 ecdf_regression <- function(x) {
-    ## ECDF function generated from dryness_script.Rmd.  This function
-    ## was generated for 7 day rolling windows and forested cover
-    ## type.  To use a different length for rolling calculations and
-    ## different cover types a different regression function should be
-    ## used here.
-    e <- exp(1)
-    return(e^(-10.38 + 29.98 * x + -45.25 * x^2 + 25.65 * x^3))
-}
+  ## ECDF function generated from dryness_script.Rmd.  This function
+  ## was generated for 7 day rolling windows and forested cover
+  ## type.  To use a different length for rolling calculations and
+  ## different cover types a different regression function should be
+  ## used here.
+  e <- exp(1)
+  return(e^(-4.1839624 + 5.5952288 * x + 0.2504363 * x^2 + -1.6207789 * x^3))
+  }
 
 start.time <- Sys.time()
 
@@ -65,29 +65,47 @@ future_files <- Sys.glob(paste0(future_path, "Deficit_rolling_sum_", rolling_win
 
 wbdata_future_smoothed <- terra::rast(future_files)
 
-wbdata_future_smoothed <- wbdata_future_smoothed %>%
-    subset(yday(time(.)) >= fire_season_start) %>%
-    subset(yday(time(.)) <= fire_season_end)
+## wbdata_future_smoothed <- wbdata_future_smoothed %>%
+##     subset(yday(time(.)) >= fire_season_start) %>%
+##     subset(yday(time(.)) <= fire_season_end)
 
-quants <- c(0.79, 0.845, 0.875)
+ncpaths_historical_smoothed <- list.files(path = hist_path, pattern = "Deficit.*.nc", full.names = TRUE)
+wbdata_historical_smoothed <- rast(ncpaths_historical_smoothed)
+
+
+replace_duplicated <- function(x) {
+  x[duplicated(x)] <- NA
+  return(x)
+}
+
+wbdata_historical_smoothed <- wbdata_historical_smoothed %>%
+  subset(year(time(.)) <= 2021) %>%
+  #### Pixellation correction adjustments
+  round(digits = 1) %>%
+  subst(0, NA) %>%
+  terra::app(function(x) replace_duplicated(x))
+
+
+quants <- c(0.35, 0.5, 0.7)
 ############
 #### Generate Quants
-## ### ecdf_regression(0.790) = 0.1012792
-## ### ecdf_regression(0.845) = 0.1525024
-## ### ecdf_regression(0.875) = 0.2001806
-## quants_string <- paste0(quants, "_", collapse = "")
-## wbdata_historical_quants_file <-  paste0(out_path, "Deficit_historical_quants_", quants_string, ".nc")
+## ### ecdf_regression(0.350) = 0.1039
+## ### ecdf_regression(0.500) = 0.2173
+## ### ecdf_regression(0.700) = 0.496
+quants_string <- paste0(quants, "_", collapse = "")
+wbdata_historical_quants_file <-  paste0(out_path, "Deficit_historical_quants_", quants_string, ".nc")
 
-## start.time <- Sys.time()
-## wbdata_historical_quants <- terra::quantile(wbdata_historical_smoothed, probs = quants, na.rm = TRUE, filename = wbdata_historical_quants_file, overwrite = TRUE)
-## end.time <- Sys.time()
-## time.taken <- round(end.time - start.time,2)
-## time.taken
+start.time <- Sys.time()
+wbdata_historical_quants <- terra::quantile(wbdata_historical_smoothed, probs = quants, na.rm = TRUE)
+end.time <- Sys.time()
+names(wbdata_historical_quants) <- quants
+writeCDF(wbdata_historical_quants, filename = wbdata_historical_quants_file, overwrite = TRUE)
+time.taken <- round(end.time - start.time,2)
+time.taken
 #############
 
-wbdata_historical_quants <- terra::rast(quants_path)
+##wbdata_historical_quants <- terra::rast(quants_path)
 
-names(wbdata_historical_quants) <- quants
 
 above_quants_0.1 <- terra::compare(wbdata_future_smoothed, subset(wbdata_historical_quants, 1), ">")
 above_quants_0.15 <- terra::compare(wbdata_future_smoothed, subset(wbdata_historical_quants, 2), ">")
