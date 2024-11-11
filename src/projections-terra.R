f55da48712fe373209702d3c47cac4c369ed41f4
Stephen Huysman <shuysman@gmail.com>
2024-11-10 17:50:09 -0700

    split out historical quants to own script

--- src/projections-terra.R
+++ src/projections-terra.R
@@ -38,7 +38,7 @@ future_path <- paste0(home_path, "/data/nps_gridded_wb/gye/forecasts/rolling_sum
 ##future_path <- paste0("/media/smithers/shuysman/data/nps_gridded_wb/gye/forecasts/rolling_sum/")
 ##hist_path <- "/media/smithers/shuysman/data/nps_gridded_wb/gye/historical/rolling_sum/"
 ##quants_path <- "/home/steve/OneDrive/nothern-rockies-dryness/out/Deficit_historical_quants_0.79_0.845_0.875_.nc"
-##quants_path <- paste0(home_path, "/data/gye/historical/rolling_sum/Deficit_historical_quants_0.35_0.5_0.7_.nc")
+quants_path <- paste0(home_path, "/data/gye/historical/rolling_sum/Deficit_historical_quants_0.35_0.5_0.7_.nc")
 hist_path <- paste0(home_path, "/data/nps_gridded_wb/gye/historical/rolling_sum/")
 ##out_path <- "/media/smithers/shuysman/data/out/fire/"
 out_path <- "./out/"
@@ -69,42 +69,9 @@ wbdata_future_smoothed <- terra::rast(future_files)
 ##     subset(yday(time(.)) >= fire_season_start) %>%
 ##     subset(yday(time(.)) <= fire_season_end)
 
-ncpaths_historical_smoothed <- list.files(path = hist_path, pattern = "Deficit.*.nc", full.names = TRUE)
-wbdata_historical_smoothed <- rast(ncpaths_historical_smoothed)
 
-
-replace_duplicated <- function(x) {
-  x[duplicated(x)] <- NA
-  return(x)
-}
-
-wbdata_historical_smoothed <- wbdata_historical_smoothed %>%
-  subset(year(time(.)) <= 2021) %>%
-  #### Pixellation correction adjustments
-  round(digits = 1) %>%
-  subst(0, NA) %>%
-  terra::app(function(x) replace_duplicated(x))
-
-
-quants <- c(0.35, 0.5, 0.7)
-############
-#### Generate Quants
-## ### ecdf_regression(0.350) = 0.1039
-## ### ecdf_regression(0.500) = 0.2173
-## ### ecdf_regression(0.700) = 0.496
-quants_string <- paste0(quants, "_", collapse = "")
-wbdata_historical_quants_file <-  paste0(out_path, "Deficit_historical_quants_", quants_string, ".nc")
-
-start.time <- Sys.time()
-wbdata_historical_quants <- terra::quantile(wbdata_historical_smoothed, probs = quants, na.rm = TRUE)
-end.time <- Sys.time()
+wbdata_historical_quants <- terra::rast(quants_path)
 names(wbdata_historical_quants) <- quants
-writeCDF(wbdata_historical_quants, filename = wbdata_historical_quants_file, overwrite = TRUE)
-time.taken <- round(end.time - start.time,2)
-time.taken
-#############
-
-##wbdata_historical_quants <- terra::rast(quants_path)
 
 
 above_quants_0.1 <- terra::compare(wbdata_future_smoothed, subset(wbdata_historical_quants, 1), ">")
